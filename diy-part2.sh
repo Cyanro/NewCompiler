83c0e3cde09aee71fd1dd220c25da64e1e6b3264
Cyanro <113330384+Cyanro@users.noreply.github.com>
2024-12-01 19:42:33 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -17,4 +17,4 @@ sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/luci2/bin/config_generat
 #sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
 # 修改默认主机名称
-sed -i 's/OpenWrt/NLEDE/g' package/base-files/luci2/bin/config_generate
+sed -i 's/LEDE/NLEDE/g' package/base-files/luci2/bin/config_generate
